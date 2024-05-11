3e823be3893fbd671b23f08e2bf82accfee37503
gauravkmjaiswal <56041706+gauravkmjaiswal@users.noreply.github.com>
2024-05-11 14:08:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+app.use(bodyParser.json());
+app.get('/ping', (req, res) => {
+    res.send('pongok');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
