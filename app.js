3ac48fa35cc9bd5db95496d01c06652920759d6b
gauravkmjaiswal <56041706+gauravkmjaiswal@users.noreply.github.com>
2024-05-11 14:34:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,69 @@ app.get('/', (req, res) => {
 
 app.get('/ping', (req, res) => {
     res.send('pong');
+    TestCase();
 });
 
+
+
+url='https://api-demo.bybit.com';
+
+var apiKey = process.env.apiKeyI;
+var secret = process.env.secretI;
+var recvWindow = 5000;
+var timestamp = Date.now().toString();
+
+function getSignature(parameters, secret) {
+    return crypto.createHmac('sha256', secret).update(timestamp + apiKey + recvWindow + parameters).digest('hex');
+}
+async function http_request(endpoint,method,data,Info) {
+    var sign=getSignature(data,secret);
+    var fullendpoint;
+
+    // Build the request URL based on the method
+    if (method === "POST") {
+        fullendpoint = url + endpoint;
+    } else {
+        fullendpoint = url + endpoint + "?" + data;
+        data = "";
+    }
+
+    var headers = {
+        'X-BAPI-SIGN-TYPE': '2',
+        'X-BAPI-SIGN': sign,
+        'X-BAPI-API-KEY': apiKey,
+        'X-BAPI-TIMESTAMP': timestamp,
+        'X-BAPI-RECV-WINDOW': recvWindow.toString()  
+    };
+
+    if (method === "POST") {
+        headers['Content-Type'] = 'application/json; charset=utf-8';
+    }
+
+    var config = {
+        method: method,
+        url: fullendpoint,
+        headers: headers,
+        data: data
+    };
+
+    console.log(Info + " Calling....");
+    await axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+}
+
+async function TestCase()
+{
+endpoint="/v5/order/create"
+const orderLinkId = crypto.randomBytes(16).toString("hex");
+var data = '{"category":"spot","symbol": "DOGEUSDT","side": "Buy","positionIdx": 0,"orderType": "Limit","qty": "1000","price": "0.14490","timeInForce": "GTC","orderLinkId": "' + orderLinkId + '"}';
+await http_request(endpoint,"POST",data,"Create");
+}
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
